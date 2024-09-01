b6cb0c006b02a2ad5bbb69008a00ab8b3f10ca79
kit <kit.mavareik@gmail.com>
2024-09-01 13:56:17 +0300

    Add PageWrapper component with styling and responsive Drawer
    Introduced a new PageWrapper component to wrap main page content. The component includes an AppBar with a toggleable Drawer, styled using Material-UI. Added skeletons for loading states within the Drawer to enhance user experience.

---
+++ src/design/templates/page-wrapper/page-wrapper.styles.ts
@@ -0,0 +1,33 @@
+import {SxProps, Theme} from "@mui/material";
+
+export const main: SxProps<Theme> = {
+    flexGrow: 1,
+}
+
+export const drawer: { container: SxProps<Theme>; item: { container: SxProps<Theme>; cover: SxProps<Theme> } } = {
+    container: {
+        width: 300,
+    },
+    item: {
+ container: {
+    display: "flex",
+        gap: 2,
+        p: theme => theme.spacing(1, 2),
+ },
+        cover: {
+     height: 48,
+            aspectRatio: 1/3
+        }
+}
+}
+
+export const skeleton: { cover: SxProps<Theme>; label: SxProps<Theme> } = {
+    cover: {
+        ...drawer.item.cover
+    },
+    label: {
+        width: 100,
+    }
+}
+
+export default { drawer, main, skeleton }
---
+++ src/design/templates/page-wrapper/page-wrapper.tsx
@@ -0,0 +1,39 @@
+import React, {ReactNode, useMemo, useState} from 'react';
+import {AppBar, Box, Button, Drawer, IconButton, Stack, Skeleton, Toolbar, Typography} from "@mui/material";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import logo from './logo.svg';
+
+import style from './page-wrapper.styles'
+
+interface PageWrapperProps {
+    children: ReactNode;
+}
+
+export function PageWrapper({children}: PageWrapperProps) {
+    const [showDrawer, setShowDrawer] = useState(false)
+
+    const drawerContent = useMemo(() => {
+        return [1, 2, 3, 4, 5].map(item => (
+            <Box key={item} sx={style.drawer.item.container}>
+                <Skeleton sx={style.skeleton.cover}/>
+                <Skeleton sx={style.skeleton.label}/>
+            </Box>
+        ))
+    }, []);
+
+    return (
+        <>
+            <Drawer open={showDrawer} onClose={() => setShowDrawer(false)}>
+                {drawerContent}
+            </Drawer>
+            <Stack gap={5}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <Button color="inherit" startIcon={<MenuRoundedIcon />} onClick={() => setShowDrawer(true)}>Movie Catalog</Button>
+                        </Toolbar>
+                    </AppBar>
+                <Box component='main' sx={style.main}>{children}</Box>
+            </Stack>
+        </>
+    );
+}
