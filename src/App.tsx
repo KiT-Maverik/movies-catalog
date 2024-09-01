b44a564b6530b4cdba6b2026d8670d9a005e8813
kit <kit.mavareik@gmail.com>
2024-09-01 14:22:32 +0300

    Add routing with React Router
    Integrated React Router for navigating between HomePage and MoviePage. Updated imports and replaced placeholder content inside PageWrapper with route-based components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
-import {PageWrapper} from "./design/templates/page-wrapper/page-wrapper";
+import {PageWrapper} from "design/templates/page-wrapper/page-wrapper";
+import {routes} from "api/constants/routes.constats";
+import {HomePage} from "design/pages/home/home";
+import {MoviePage} from "design/pages/movie/movie";
+
 
 function App() {
   return (
-      <PageWrapper>kjhkkhkjhjhk</PageWrapper>
+      <PageWrapper>
+        <Router>
+            <Switch>
+              <Route path={routes.home}>
+                <HomePage />
+              </Route>
+              <Route path={routes.movie}>
+                <MoviePage />
+              </Route>
+            </Switch>
+        </Router>
+      </PageWrapper>
   );
 }
 
