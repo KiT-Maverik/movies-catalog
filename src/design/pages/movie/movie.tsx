1ad40d825aab00109f6fa429bb813bb8f0cadb56
kit <kit.mavareik@gmail.com>
2024-09-01 19:26:59 +0300

    Add movie query to MoviePage component
    Integrate `useGetMovieByIdQuery` to fetch movie details in `MoviePage`. Return null while the movie data is loading to prevent rendering incomplete content.

--- src/design/pages/movie/movie.tsx
+++ src/design/pages/movie/movie.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import {Box, Container} from "@mui/material";
 
 import style from './movie.styles'
+import {useGetMovieByIdQuery} from "api/queries/movies/getMovieById.query";
 
 export function MoviePage() {
+    const {loading, movie} = useGetMovieByIdQuery()
+
+    if (loading) return null
+
     return (
         <Container sx={style.container}>
             <Box sx={style.cover}>cover</Box>
