0c7b2e3b2171e85b5928b03e2c6f5eb98d2bca5f
kit <kit.mavareik@gmail.com>
2024-09-02 07:33:32 +0300

    Refactor movie state initialization and filtering logic
    Changed the initialization of the movie state to use a typed Movie entity. Replaced the use of filter with find to directly get the movie object by ID. This improves type safety and performance.

--- src/api/queries/movies/getMovieById.query.ts
+++ src/api/queries/movies/getMovieById.query.ts
@@ -2,17 +2,18 @@ import {useCallback, useState} from "react";
 import axios from "axios";
 import {endpoint} from "api/constants/endpoints.constats";
 import {movies} from "../../constants/mock.constants";
+import {Movie} from "../../contracts/movie/entities/entities";
 import {delay} from "../../../constants/environment.constants";
 
 export const useGetMovieByIdQuery = () => {
     const [loading, setLoading] = useState(false)
-    const [movie, setMovie] = useState({})
+    const [movie, setMovie] = useState<Movie>()
 
     const getMovieById = useCallback((id: number) => {
         setLoading(true)
 
         setTimeout(() => {
-            setMovie(movies.filter((item) => item.id === id));
+            setMovie(movies.find((item) => item.id === id));
             setLoading(false);
         }, delay);
     }, []);
