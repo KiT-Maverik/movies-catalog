3c1789da3de8dabe3b9a426ecc3ddca44dae9a64
kit <kit.mavareik@gmail.com>
2024-09-14 18:19:49 +0300

    Refactor HomePage component to use Page template
    Replace Container with Page in HomePage component for better layout consistency. Removed unnecessary Container import and updated styling accordingly.

--- src/design/pages/HomePage/HomePage.tsx
+++ src/design/pages/HomePage/HomePage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import {Container, Typography} from "@mui/material";
+import {Typography} from "@mui/material";
 import MovieCreationRoundedIcon from '@mui/icons-material/MovieCreationRounded';
+
+import {Page} from "design/templates";
 
 import style from './HomePage.styles'
 
 export function HomePage() {
     return (
-        <Container sx={style.container}>
+        <Page>
             <MovieCreationRoundedIcon color="disabled" sx={style.icon}/>
             <Typography variant="h5" color="text.disabled" textTransform="capitalize">Choose a movie to see the details</Typography>
-        </Container>
+        </Page>
     );
 }
