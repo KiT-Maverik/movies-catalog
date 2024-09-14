10416bba4bb505ffe8673149f0d95d114ae48028
kit <kit.mavareik@gmail.com>
2024-09-14 20:09:07 +0300

    Refactor delete movie mutation
    Modified the useDeleteMovieMutation function to accept the movie's ID directly in the mutation function instead of as a parameter to the hook. This change simplifies the signature and usage of the mutation hook.

--- src/api/mutations/useDeleteMovieMutation.ts
+++ src/api/mutations/useDeleteMovieMutation.ts
@@ -2,11 +2,11 @@ import {useMutation, useQueryClient} from "@tanstack/react-query";
 
 import {queryKey, endpoint, api} from "api";
 
-export const useDeleteMovieMutation = (id: number) => {
+export const useDeleteMovieMutation = () => {
     const queryClient = useQueryClient()
 
     const deleteMovieMutation = useMutation({
-        mutationFn: async () => await api.delete(endpoint.movie.getById(id)),
+        mutationFn: async (id: number) => await api.delete(endpoint.movie.getById(id)),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: [queryKey.getMoviesList] })
         },
