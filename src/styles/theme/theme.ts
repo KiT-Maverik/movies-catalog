c551f5a9fe3da12e50f5d44492a159becaa030e7
kit <kit.mavareik@gmail.com>
2024-09-14 18:19:25 +0300

    Add mixins to theme configuration
    Included mixins in the theme configuration to enhance styling capabilities. This change will allow the reuse of common style patterns throughout the application.

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -2,10 +2,12 @@ import { createTheme } from "@mui/material/styles";
 
 import { breakpoints } from "./breakpoints";
 import { shape } from "./shape";
+import {mixins} from "./mixins";
 
 export const theme = () =>
   createTheme({
     breakpoints,
+      mixins,
     shape,
     spacing: 4,
   });
