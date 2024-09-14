17effa6d8564fad635a7a8b3e1185fc14a0cf329
kit <kit.mavareik@gmail.com>
2024-09-14 12:56:29 +0300

    Refactor getMovieById to use API call instead of mock data
    Replaced the timeout and mock data with an actual API call using axios to fetch movie details by ID. This ensures data accuracy and reflects the current state from the server instead of relying on hardcoded values.

--- src/api/queries/movies/getMovieById.query.ts
+++ src/api/queries/movies/getMovieById.query.ts
@@ -1,21 +1,18 @@
 import {useCallback, useState} from "react";
 import axios from "axios";
 import {endpoint} from "api/constants/endpoints.constats";
-import {movies} from "../../constants/mock.constants";
-import {Movie} from "../../contracts/movie/entities/entities";
-import {delay} from "../../../constants/environment.constants";
+import {Movie} from "api/contracts/movie/entities/entities";
 
 export const useGetMovieByIdQuery = () => {
     const [loading, setLoading] = useState(false)
     const [movie, setMovie] = useState<Movie>()
 
-    const getMovieById = useCallback((id: number) => {
+    const getMovieById = useCallback((id: string) => {
         setLoading(true)
-
-        setTimeout(() => {
-            setMovie(movies.find((item) => item.id === id));
-            setLoading(false);
-        }, delay);
+        axios
+            .get<Movie>(endpoint.movie.getById(id))
+            .then((rs) => setMovie(rs.data))
+            .finally(() => setLoading(false))
     }, []);
 
     return {loading, movie, getMovieById }
