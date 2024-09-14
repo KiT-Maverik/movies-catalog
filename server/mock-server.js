16d9be1d1598f4b5fed4d61e16d91c184e9322ce
kit <kit.mavareik@gmail.com>
2024-09-14 12:46:15 +0300

    Add PATCH and DELETE endpoints to mock server
    Implemented PATCH endpoint to update movie details by ID, and DELETE endpoint to remove a movie by ID. Adjusted movies constant to be mutable for these operations.

--- server/mock-server.js
+++ server/mock-server.js
@@ -1,11 +1,24 @@
 const express = require('express')
-const movies = require('./data/movies.data')
+let movies = require('./data/movies.data')
 const app = express()
 const port = 3001
 
 app.get('/movies', (req, res) => {
     res.json(movies)
 })
+
+app.patch('/movies/:id', (req, res) => {
+    movies = movies.map(movie => {
+        if (movie.id === parseInt(req.params.id, 10)) return {...movie, ...req.body}
+        else return movie
+    });
+    res.status(200).json({status: 'Movie updated'});
+});
+
+app.delete('/movies/:id', (req, res) => {
+    movies = movies.filter(movie => movie.id !== parseInt(req.params.id, 10));
+    res.status(204);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
