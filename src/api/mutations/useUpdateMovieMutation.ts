b8323ba6b3fb133f39f8169c5d6d9d6529ef882e
kit <kit.mavareik@gmail.com>
2024-09-16 12:02:44 +0300

    Separate invalidateQueries for movie list and movie details
    Previously, both queries were invalidated in a single call, which could have led to inconsistent cache updates. Now, they are handled separately to ensure each cache entry is updated independently. This improves cache consistency and reliability.

--- src/api/mutations/useUpdateMovieMutation.ts
+++ src/api/mutations/useUpdateMovieMutation.ts
@@ -11,7 +11,8 @@ export const useUpdateMovieMutation = () => {
     const updateMovieMutation = useMutation({
         mutationFn: async ({id, title}: NewType) => await api.patch(endpoint.movie.getById(id), {title}),
         onSuccess: (_, {id}) => {
-            queryClient.invalidateQueries({ queryKey: [queryKey.getMoviesList, queryKey.getMovieById(id)] })
+            queryClient.invalidateQueries({ queryKey: [queryKey.getMoviesList] })
+            queryClient.invalidateQueries({ queryKey: [queryKey.getMovieById(id)] })
         },
     })
 
