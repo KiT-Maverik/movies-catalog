022fe984619d3a1c0d16c9fca85744c495627775
kit <kit.mavareik@gmail.com>
2024-09-16 11:54:38 +0300

    Remove unused styles and integrate form handling in EditMovieModal
    Deleted the unused 'EditMovieModal.styles.ts' file to clean up the codebase. Integrated form handling and API interactions in 'EditMovieModal.tsx', including formik for form state management and mutation hooks for API communication.

--- src/design/pages/MoviePage/EditMovieModal/EditMovieModal.tsx
+++ src/design/pages/MoviePage/EditMovieModal/EditMovieModal.tsx
@@ -1,10 +1,12 @@
 import CancelIcon from '@mui/icons-material/CloseRounded';
 import SaveIcon from '@mui/icons-material/CheckRounded';
-import {Button} from "@mui/material";
+import {Button, TextField} from "@mui/material";
+import {useFormik} from "formik";
+import {useParams} from "react-router-dom";
 
+import {useGetMovieByIdQuery, useUpdateMovieMutation} from "api";
 import {Modal} from "design/templates";
-
-import style from './EditMovieModal.styles'
+import {useToast} from "App";
 
 interface EditMovieModalProps {
     open: boolean
@@ -12,15 +14,41 @@ interface EditMovieModalProps {
 }
 
 export const EditMovieModal = ({onClose, open}: EditMovieModalProps) => {
+    const { movieId } = useParams();
+    const { showToast } = useToast();
+    const {getMovieByIdQuery} = useGetMovieByIdQuery(parseInt(movieId || ""))
+    const { updateMovieMutation } = useUpdateMovieMutation();
+
+    const {handleBlur, handleChange, values, submitForm} = useFormik({
+        initialValues: {
+            title: getMovieByIdQuery.data?.data.title,
+        },
+        onSubmit: (values) => {
+            updateMovieMutation.mutateAsync({
+                id: parseInt(movieId || ""),
+                title: values.title}, {onSuccess: () => {
+                    showToast({type: 'success', message: 'Movie updated'})
+                    onClose();
+                }
+            })
+        },
+    });
+
     return (
         <Modal open={open} onClose={onClose}>
             <Modal.Header title="Edit movie" onClose={onClose}/>
             <Modal.Body>
-
+                <TextField label='Title'
+                           name='title'
+                           value={values.title}
+                           onChange={handleChange}
+                           onBlur={handleBlur}
+                           defaultValue={getMovieByIdQuery.data?.data.title}
+                />
             </Modal.Body>
             <Modal.Actions>
                 <Button startIcon={<CancelIcon/>} onClick={onClose} variant='text'>Cancel</Button>
-                <Button startIcon={<SaveIcon/>} variant='contained'>Save</Button>
+                <Button startIcon={<SaveIcon/>} variant='contained'onClick={submitForm}>Save</Button>
             </Modal.Actions>
         </Modal>
     )
