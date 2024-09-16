d5a2b061bdfc7d2f5eea6d96341a1dcf9c1e87ef
kit <kit.mavareik@gmail.com>
2024-09-16 13:16:13 +0300

    Switch registry URLs from yarnpkg to npmjs
    Updated registry URL for several dependencies in yarn.lock from yarnpkg.com to npmjs.org. This ensures consistency in package sources and might help in resolving any potential discrepancies.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
-// @ts-check
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tsEsLint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginPrettier from "eslint-plugin-prettier/recommended";
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-);
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  pluginJs.configs.recommended,
+  ...tsEsLint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginPrettier,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
