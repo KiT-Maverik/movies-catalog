7ddf920efad92e50675f840820db62ea4b6a3063
kit <kit.mavareik@gmail.com>
2024-09-17 11:30:03 +0300

    Add testing constants export
    Introduce `testing.constants.ts` to hold constants for testing purposes and update `index.ts` to export these newly added constants. This change is to enhance modularity and maintainability of testing configurations.

---
+++ src/App/constants/testing.constants.ts
@@ -0,0 +1,3 @@
+export const testSuite = {
+  contractValidation: "Contract Validation",
+};
--- src/App/index.ts
+++ src/App/index.ts
@@ -4,3 +4,4 @@ export * from "./ToastProvider/ToastProvider.hooks";
 // Constants
 export * from "./constants/environment.constants";
 export * from "./constants/routes.constats";
+export * from "./constants/testing.constants";
